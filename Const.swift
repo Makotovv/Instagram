b2f40c09f7e089f31f8c2dd4bddb49cf2183be4a
Makoto.Kaneko <mako_080809-xue@yahoo.co.jp>
2018-10-11 02:06:24 +0900

    1010の最終＜最新＞

---
+++ Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Makoto Kaneko on 2018/10/11.
+//  Copyright © 2018年 Makoto Kaneko. All rights reserved.
+//
+
+import Foundation
+
+    struct Const {
+        static let PostPath = "posts"
+
+}
